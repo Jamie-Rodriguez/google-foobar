1b0e04b81517c0a5d796b81f701fe8a2029ba200
Jamie Rodriguez <>
2022-10-01 23:01:31 +0200

    Make Stage 3: Prepare the Bunnies' Escape clearer to understand

--- prepare-the-bunnies-escape/solution.py
+++ prepare-the-bunnies-escape/solution.py
@@ -21,24 +21,27 @@ def neighbors(map, coord):
 
 def bfs(map, num_walls):
     # type: (List[List[int]], int) -> Optional[int]
-    # Assumes 'map' is n rows, where each and every row is *always* m = len(map[0]) columns
+    # Assumes 'map' is n rows, where each and every row is *always*
+    # m = len(map[0]) columns
     # i.e. a square matrix, and not a staggered array of arrays
     # As per Read-Me problem description
     num_rows = len(map)
     # Not strictly necessary, but a little more safe
     num_columns = len(map[num_rows - 1])
     # Problem description in Read-Me:
-    # "The door out of the station is at the top left (0,0) and the door into an escape pod is at the bottom right (w-1,h-1)."
+    # "The door out of the station is at the top left (0,0) and the door into
+    # an escape pod is at the bottom right (w-1,h-1)."
     start = (0, 0)
     end = (num_rows - 1, num_columns - 1)
 
     # The current, minimum number of walls we have gone through to reach
     # arbitrary square: wall_count_last_visit[r][c].
-    # We initialise all squares to num_walls so that the search will
+    # We initialise all squares to 'num_walls' so that the search will
     # then prioritise going through *less* walls than this number,
     # and conversely will never go through more walls than this number
-    wall_count_last_visit = [[num_walls for j in range(num_columns)] for i in range(num_rows)]
-    # Tuples of (coordinate, number of walls we have gone through, number of steps taken so far)
+    wall_count_last_visit = [[num_walls for _ in range(num_columns)] for _ in range(num_rows)]
+    # Tuples of
+    #     (coordinate, number of walls we have gone through, number of steps taken so far)
     queue = deque()
     queue.appendleft((start, 0, 0))
 
@@ -55,11 +58,12 @@ def bfs(map, num_walls):
 
         for (next_row, next_column) in neighbors(map, current):
             # Did we find a new path that goes through less walls than our search so far?
-            if wall_count_last_visit[next_row][next_column] >= current_wall_count:
+            if current_wall_count <= wall_count_last_visit[next_row][next_column]:
                 wall_count_last_visit[next_row][next_column] = current_wall_count
                 queue.appendleft(((next_row, next_column), current_wall_count, num_steps + 1))
 
-    # I made this case return None, but if we wanted to strictly adhere to the type signature
+    # I made this case return None, but if we wanted to strictly adhere to the
+    # type signature
     #     def solution(map): (List[List[int]]) -> int
     # Then we could return -1 or 0 or something instead
     # The problem definition already states that all mazes are solvable anyway
@@ -79,39 +83,39 @@ def solution(map): # type: (List[List[int]]) -> int
 assert solution([[0, 1, 1, 0],
                  [0, 0, 0, 1],
                  [1, 1, 0, 0],
-                 [1, 1, 1, 0]]) == 7, 'solution([[0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 0, 0], [1, 1, 1, 0]]) failed!'
+                 [1, 1, 1, 0]]) == 7
 assert solution([[0, 0, 0, 0, 0, 0],
                  [1, 1, 1, 1, 1, 0],
                  [0, 0, 0, 0, 0, 0],
                  [0, 1, 1, 1, 1, 1],
                  [0, 1, 1, 1, 1, 1],
-                 [0, 0, 0, 0, 0, 0]]) == 11, 'solution([[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]]) failed!'
+                 [0, 0, 0, 0, 0, 0]]) == 11
 
 # =========== My own, unofficial test cases to help debug behaviour ===========
 assert solution([[0, 0],
-                 [0, 1]]) == 3, 'solution([[0, 0], [0, 1]]) failed!'
+                 [0, 1]]) == 3
 # Although the Read-Me states that "The map will always be solvable",
 # It isn't too hard to account for this
 assert solution([[0, 1],
-                 [1, 1]]) == None, 'solution([[0, 1], [1, 1]]) failed!'
+                 [1, 1]]) == None
 assert solution([[0, 1, 0],
                  [0, 1, 1],
-                 [0, 1, 0]]) == 5, 'solution([[0, 1, 0], [0, 1, 1], [0, 1, 0]]) failed!'
+                 [0, 1, 0]]) == 5
 assert solution([[0, 1, 1],
-                 [0, 1, 0]]) == 4, 'solution([[0, 1, 1], [0, 1, 0]]) failed!'
+                 [0, 1, 0]]) == 4
 assert solution([[0, 1, 0, 0, 0, 1],
-                 [0, 0, 0, 1, 0, 0]]) == 7, 'solution([[0, 1, 0, 0, 0, 1], [0, 0, 0, 1, 0, 0]]) failed!'
+                 [0, 0, 0, 1, 0, 0]]) == 7
 assert solution([[0, 1, 0, 0, 0],
                  [0, 1, 0, 1, 0],
                  [0, 0, 0, 1, 1],
-                 [0, 0, 1, 1, 0]]) == 12, 'solution([0, 1, 0, 0, 0], [0, 1, 0, 1, 0], [0, 0, 0, 1, 1], [0, 0, 1, 1, 0]) failed!'
+                 [0, 0, 1, 1, 0]]) == 12
 assert solution([[0, 0, 0, 0],
                  [1, 1, 1, 0],
                  [0, 0, 0, 0],
                  [0, 1, 0, 1],
-                 [0, 0, 0, 0]]) == 8, 'solution([[0, 0, 0, 0], [1, 1, 1, 0], [0, 0, 0, 0], [0, 1, 0, 1], [0, 0, 0, 0]]) failed!'
+                 [0, 0, 0, 0]]) == 8
 assert solution([[0, 1, 0, 0, 0],
                  [0, 0, 0, 1, 0],
                  [0, 0, 1, 1, 0],
                  [0, 1, 1, 0, 0],
-                 [0, 1, 1, 0, 0]]) == 9, 'solution([[0, 1, 0, 0, 0], [0, 0, 0, 1, 0], [0, 0, 1, 1, 0], [0, 1, 1, 0, 0], [0, 1, 1, 0, 0]]) failed!'
+                 [0, 1, 1, 0, 0]]) == 9
